0b6a7e72cefc099bfa5bbcdd04fb537e45db0ffa
satya <satya@192.168.3.204>
2021-10-13 12:12:58 +0530

    create class and add two variable

---
+++ java/src/java/Test.java
@@ -0,0 +1,14 @@
+package java;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b= 5;
+		int c = a-b;
+		System.out.println(c);
+
+	}
+
+}
